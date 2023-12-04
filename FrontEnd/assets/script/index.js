4e2326750fb3927329b6091743dbfb000e224ae1
samuel <samuel.baranowski@orange.fr>
2023-12-04 07:58:24 +0100

    feat : boutons pour les filtres des categories

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -1,4 +1,4 @@
-// URL de l'API
+// URL de l'API pour recuperer les works
 const apiWorks = 'http://localhost:5678/api/works';
 
 // Div avec l'ID "gallery" dans l'HTML pour pouvoir afficher les éléments.
@@ -37,8 +37,30 @@ fetch(apiWorks)
             galleryDiv.appendChild(figureElement);
         });
     })
-    
+
+
     // Message d'erreur si les données ne peuvent être récupérées
     .catch(error => {
         console.error('Erreur lors de la récupération des données:', error);
+    });
+
+// URL de l'API pour récupérer les catégories
+const apiCategories = 'http://localhost:5678/api/categories';
+
+// Utiliser Fetch pour effectuer une requête GET
+fetch(apiCategories)
+    .then(response => {
+        // Vérifier si la requête a réussi (statut 200)
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+        }
+        // Réponse JSON
+        return response.json();
+    })
+    .then(categories => {
+        // Les categories sont recuperes
+        console.log(categories);
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des catégories:', error);
     });
